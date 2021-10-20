d2e8d1987bab674061d1fb566045ab30bd6c686a
C Valdes SMA <cesar.valdes@colegiosma.com>
2021-10-20 20:39:47 +0200

    primera subida examen

---
+++ src/examen/Ej4.java
@@ -0,0 +1,11 @@
+package examen;
+
+public class Ej4 {
+
+	public static void main(String[] args) {
+		//Escribir todos los números del 100 al 0 de 5 en 5
+		for (int i = 100; i>=0; i=i-5) {
+			System.out.println(i);
+		}
+	}
+}
