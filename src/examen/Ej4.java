6c644d149d750fd044f4f866ae64be56e1b0d09f
C Valdes SMA <cesar.valdes@colegiosma.com>
2021-10-22 20:12:21 +0200

    soy cesar y he cambiado el for por el while infinito

--- src/examen/Ej4.java
+++ src/examen/Ej4.java
@@ -4,23 +4,8 @@ public class Ej4 {
 
 	public static void main(String[] args) {
 		//Escribir todos los números del 100 al 0 de 5 en 5
-		for (int i = 100; i>=0; i=i-5) {
-			System.out.println(i);
-			
-			
-			System.out.println(i);
-			System.out.println(i);
-			System.out.println(i);
-			System.out.println(i);
-		
-			System.out.println(i);
-			System.out.println(i);
-			
-			
-			System.out.println(i);
-			System.out.println(i);
-			System.out.println(i);
-			System.out.println(i);
+		while(true==true){
+			System.out.println("Soy más tonto que yo que se que");
 		}
 	}
 }
