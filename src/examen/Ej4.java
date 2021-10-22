e39c7005ef1b5970ca85e358baa4513f859d9897
C Valdes SMA <cesar.valdes@colegiosma.com>
2021-10-22 20:02:57 +0200

    cambio que yo quería

--- src/examen/Ej4.java
+++ src/examen/Ej4.java
@@ -6,6 +6,12 @@ public class Ej4 {
 		//Escribir todos los números del 100 al 0 de 5 en 5
 		for (int i = 100; i>=0; i=i-5) {
 			System.out.println(i);
+			
+			
+			System.out.println(i);
+			System.out.println(i);
+			System.out.println(i);
+			System.out.println(i);
 		}
 	}
 }
